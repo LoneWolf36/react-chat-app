f8d9cfb0416183c09d2d29697cad299d1c84a167
LoneWolf36 <runefaizan36@gmail.com>
2018-06-12 10:54:06 +0530

    Added messaging functionality and joining a room

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit'
+import MessageList from './MessageList'
 
 class ChatScreen extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            messages: []
+        }
+    }
+
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: 'v1:us1:f1ae7877-7bb2-4d97-bb02-d24cd63535a8',
+            userId: this.props.currentUsername,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: 'http://localhost:3001/authenticate'
+            })
+        })
+
+        chatManager
+            .connect()
+            .then(currentUser => {
+                return currentUser.subscribeToRoom({
+                    roomId: 9259313,
+                    messageLimit: 100,
+                    hooks: {
+                        onNewMessage: message => {
+                            this.setState({ messages: [...this.state.messages, message],
+                            })
+                        }
+                    }
+                })
+            })
+            .then(currentRoom => {})
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     render() {
         return (
-            <h1>Chat</h1>
+            <MessageList messages={this.state.messages} />
         )
     }
 }
--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 class UsernameForm extends React.Component {
     constructor(props) {
         super(props)
